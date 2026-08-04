c7e04b74e6c9fd566bee0a47d69e861af27de931
eisundinnig <mringhoff@gmx.de>
2026-08-04 17:33:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-fetch("https://i3s6iw.podcaster.de/eisinnig.rss")
-    .then(response => response.text())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error(error);
-    });
+
